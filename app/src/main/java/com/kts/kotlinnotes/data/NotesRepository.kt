90d228fd549eab4e617f15698c262b984a0baad8
mpolyakov <polyakov4@yandex.ru>
2020-09-21 15:42:14 +0300

    добавление облачного хранилища заметок Firestore

--- app/src/main/java/com/kts/kotlinnotes/data/NotesRepository.kt
+++ app/src/main/java/com/kts/kotlinnotes/data/NotesRepository.kt
@@ -5,25 +5,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.kts.kotlinnotes.data.entity.Note
 import com.kts.kotlinnotes.data.model.NoteResult
+import com.kts.kotlinnotes.data.provider.FireStoreProvider
 import com.kts.kotlinnotes.data.provider.RemoteDataProvider
 import java.util.*
 
 object NotesRepository {
 
-    private val remoteProvider: RemoteDataProvider = object : RemoteDataProvider {
-        override fun subscribeToAllNotes(): LiveData<NoteResult> {
-            return MutableLiveData<NoteResult>()
-        }
-
-        override fun getNoteById(id: String): LiveData<NoteResult> {
-            return MutableLiveData<NoteResult>()
-        }
-
-        override fun saveNote(note: Note): LiveData<NoteResult> {
-            return MutableLiveData<NoteResult>()
-        }
-
-    }
+    private val remoteProvider: RemoteDataProvider = FireStoreProvider()
 
     fun getNotes(): LiveData<NoteResult> = remoteProvider.subscribeToAllNotes()
     fun saveNote(note: Note) : LiveData<NoteResult> = remoteProvider.saveNote(note)
---
+++ app/src/main/java/com/kts/kotlinnotes/data/provider/FireStoreProvider.kt
@@ -0,0 +1,58 @@
+package com.kts.kotlinnotes.data.provider
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QueryDocumentSnapshot
+import com.kts.kotlinnotes.data.entity.Note
+import com.kts.kotlinnotes.data.model.NoteResult
+
+class FireStoreProvider : RemoteDataProvider {
+
+    companion object {
+        private const val NOTES_COLLECTION = "notes"
+    }
+
+    private val store:FirebaseFirestore by lazy { FirebaseFirestore.getInstance() }
+    private val noteReference = store.collection(NOTES_COLLECTION)
+
+    override fun subscribeToAllNotes(): LiveData<NoteResult> {
+        val result = MutableLiveData<NoteResult>()
+        noteReference.addSnapshotListener { snapshot, e ->
+            e?.let {
+                result.value = NoteResult.Error(e)
+            } ?: let {snapshot?.let { val notes: MutableList<Note> = mutableListOf<Note>()
+                for (doc: QueryDocumentSnapshot in snapshot) {
+                    notes.add(doc.toObject(Note::class.java))
+                }
+                result.value = NoteResult.Success(notes) }
+
+            }
+        }
+        return result
+    }
+
+    override fun getNoteById(id: String): LiveData<NoteResult> {
+        val result = MutableLiveData<NoteResult>()
+        noteReference.document(id).get()
+                .addOnSuccessListener { snapshot -> result.value = NoteResult.Success(snapshot.toObject(Note::class.java)) }
+                .addOnFailureListener {
+                    result.value = NoteResult.Error(it)
+                }
+
+        return result
+    }
+
+    override fun saveNote(note: Note): LiveData<NoteResult> {
+        val result = MutableLiveData<NoteResult>()
+        noteReference.document(note.id).set(note)
+                .addOnSuccessListener {
+                    result.value = NoteResult.Success(note)
+                }
+                .addOnFailureListener {
+                    result.value = NoteResult.Error(it)
+                }
+
+        return result
+    }
+}
